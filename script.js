c7b08232abb537544cafdf0d89b60aeed42b1978
Sarah R <sarah@wanafam.ly>
2019-08-01 16:00:38 -0700

    quick tidy of docs

--- script.js
+++ script.js
@@ -1,52 +1,57 @@
-// Palettes & Settings Notes
-const yurple = [
-  'goldenrod', 'gold', 'moccasin', 'plum', 'violet', 'purple'
-]; // .gamma(6).mode('lrgb')
-
+/* --- Palettes & Settings Notes --- */
 // Scale brewer palettes: BuPu, RdPu, Spectral, Set3, YlGnBu
 // Modes - lrgb (rich & bright), lab (clear, good contrast)
 
 // These *work*
 // chroma.scale('YlGnBu').gamma(10).classes(32)
 // chroma.scale('YlGnBu').padding([-0.5, 0.25]).gamma(5).mode('lab') @ 1k iterations
-  
+// chroma.bezier(['darkred', 'deeppink', 'lightyellow', 'lightgreen', 'teal']).scale().gamma(5)
+// chroma.scale('YlGnBu').padding([-0.5, 0.25]).gamma(5).mode('lab')//.classes(32)//.gamma(6).mode('lrgb')
+// const yurple = ['goldenrod', 'gold', 'moccasin', 'plum', 'violet', 'purple']; // .gamma(6).mode('lrgb')
+// chroma.scale(yurple).gamma(6).mode('lrgb')  
 
 
-// utils
-const q = (query) => document.querySelector(query);
+/* --- Utilities & Dependencies --- */
 const chroma = window.chroma;
+
+const q = (query) => document.querySelector(query);
+
 const scaleToRange = (val, fromRange = [0, 500], toRange = [0, 1]) => {
   const diff = fromRange[1] - fromRange[0];
-  const scaledDiff = toRange[1] - toRange[0]
+  const scaledDiff = toRange[1] - toRange[0];
   
   return scaledDiff * (val - fromRange[0]) / diff + toRange[0];
 };
+
 const getColor = (cell, iterations) => {
   const { isAlive, deaths, lastHue } = cell;
-  const colorScale = chroma
-    //.bezier(['darkred', 'deeppink', 'lightyellow', 'lightgreen', 'teal'])
-    .scale()
-    .gamma(5)
-    //.padding([0,0.2])
-  //chroma.scale('YlGnBu').padding([-0.5, 0.25]).gamma(5).mode('lab')//.classes(32)//.gamma(6).mode('lrgb')
+  const colorScale = chroma;
+    .scale('Spectral')
+    .gamma(6)
+    .mode('lrgb');
   const value = (iterations % 5 === 0 ) ? deaths / iterations : lastHue;
-  const scaledColor = colorScale(value)
+  const scaledColor = colorScale(value);
+
   //console.log(`SCALED COLOR RETURNED: ${scaledColor} FROM VALUE: ${value} = ${cell.deaths} / ${iterations}`);
   return lastHue ? chroma.blend(scaledColor, lastHue, 'lighten') : scaledColor;
 };
 
-// constants
-const MAX_GENERATIONS   = 5000;    // stop rendering new steps after this iteration
-const CELL_SIZE         = 5     // Size of each cell in pixels - also from viewport
-const CELL_SHADOW_SIZE  = 0;     // Size of the shadow around each cell
+/* --- Constants --- */
+const MAX_GENERATIONS   = 500;    // stop rendering new steps after this iteration
+const CELL_SIZE         = 5       // Size of each cell in pixels - also from viewport
+const CELL_SHADOW_SIZE  = 0;      // Size of the shadow around each cell
 const WINDOW_HEIGHT     = window.innerHeight;
 const WINDOW_WIDTH      = window.innerWidth;
-const SIMULATION_SIZE   = WINDOW_WIDTH / CELL_SIZE;    // TODO - should come from viewport
+const SIMULATION_SIZE   = WINDOW_WIDTH / CELL_SIZE; // TODO - should come from viewport
 const PALETTE           = chroma.scale();
-const REFRESH_MS        = 0;  // How often to re-render simulation in ms (1000ms = 1s)
-const DEFAULT_GRID_ITEM = { isAlive: false, deaths: 0, lastHue: undefined };
+const REFRESH_MS        = 0;      // How often to re-render simulation in ms (1000ms = 1s)
+const DEFAULT_GRID_ITEM = { 
+  isAlive: false, 
+  deaths: 0, 
+  lastHue: undefined 
+};
 
-// state
+/* --- State --- */
 let SIMULATION_RUNNING  = true;   // on/off toggle
 let SIMULATION_INSTANCE = null;   // will contain the setInterval render loop instance
 let SIMULATION_CELL_MAX = SIMULATION_SIZE * SIMULATION_SIZE;
@@ -54,9 +59,7 @@ let GENERATIONS         = 1;      // # of iterations
 let GRID                = null;   // grid data, 2d array of objs
 
 
-// rendering
-
-
+/* --- Rendering --- */
 // set up & initialize grid
 const gameEl = q('#gol');
 
@@ -67,7 +70,7 @@ GRID = create_grid();
 gameEl.style.width = `${CELL_SIZE}px`;
 gameEl.style.height = `${CELL_SIZE}px`;
 
-function updateLoading() {
+function updateLoading () {
   const loaded  = SIMULATION_SIZE * SIMULATION_SIZE * GENERATIONS;
   const total   = SIMULATION_SIZE * SIMULATION_SIZE * MAX_GENERATIONS;
   
@@ -101,7 +104,7 @@ const runSim = setInterval(() => {
 }, REFRESH_MS);
 
 
-// load handling
+/* --- load Handling (disabled) --- */
 // q('#progress').onclick = () => {
 //   // if (!SIMULATION_INSTANCE) {
 //   //   SIMULATION_INSTANCE = setInterval(runSimulation, REFRESH_MS);
@@ -121,7 +124,7 @@ const runSim = setInterval(() => {
 //   SIMULATION_INSTANCE = setInterval(runSimulation, REFRESH_MS);
 // }
 
-// controls
+/* --- Controls (disabled) --- */
 // q('#step').onclick = () => {
 //   // if (!SIMULATION_INSTANCE) {
 //   //   SIMULATION_INSTANCE = setInterval(runSimulation, REFRESH_MS);
@@ -161,9 +164,9 @@ const runSim = setInterval(() => {
 // };
 
 
-// Grid Fns
+/* --- Grid Functions --- */
 function create_grid(opts = {}, currentGrid = []) {
-  let gridData = []//[...currentGrid];
+  let gridData = [];//[...currentGrid];
   
   for (let y = 0; y < SIMULATION_SIZE; y++) {
     let column = [];
@@ -257,9 +260,9 @@ function checkNeighbor(x, y) {
   return 0;
 }
 
-// Drawing
+/* --- Drawing --- */
 function draw_grid() {
-  //console.log(`--- Draw grid called on iteration: ${GENERATIONS} ---`);
+  // console.log(`--- Draw grid called on iteration: ${GENERATIONS} ---`);
   // console.log('grid data: ', GRID);
 
   let boxshadows = [];
